30067f26097e8046842b6a3591f814a7c0c326b8
Dmytro Pahuba <dpaguba@gmail.com>
2023-12-27 20:57:00 +0100

    feat: move the projects and the skills into a data module

---
+++ src/data/projects.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    slug: 'kwh-4-0',
+    title: 'KWH 4.0',
+    image: '/assets/jpeg/forest-macbook.jpg',
+    alt: 'A laptop showing a path through a mixed forest in autumn',
+    width: 1895,
+    height: 1139,
+    card:
+      'The competence centre that puts Industry 4.0 into forestry. Two publicly funded research projects, one shared digital twin platform, and the applications a forester actually holds.',
+  },
+  {
+    slug: 'kherson-help-bot',
+    title: 'Kherson Help Bot',
+    image: '/assets/jpeg/kherson-bot-mockup.jpg',
+    alt: 'Three phone screens showing the intake and volunteer conversations',
+    width: 1895,
+    height: 1139,
+    card:
+      'Two Telegram bots written in the days after the Kakhovka dam collapse, one taking requests for evacuation, one handing them to the volunteers who could answer them.',
+  },
+  {
+    slug: 'fx-calendar-bot',
+    title: 'FX Calendar Bot',
+    image: '/assets/jpeg/fx-bot-mockup.jpg',
+    alt: "A Discord channel showing the bot's morning economic calendar digest",
+    width: 1895,
+    height: 1139,
+    card:
+      'A Discord bot that posts the morning economic calendar and fires a reminder fifteen minutes before every high impact event.',
+  },
+]
+
+export const skills = [
+  'HTML', 'CSS', 'SASS', 'JavaScript', 'TypeScript', 'Dart', 'Flutter', 'Android',
+  'iOS', 'Python', 'Node.js', 'NumPy', 'Plotly Dash', 'Java', 'C++', 'Kotlin', 'SQL',
+  'PostgreSQL', 'MongoDB', 'Docker', 'Git', 'Firebase', 'REST APIs', 'Riverpod',
+  'Hive', 'Figma', 'Scrum',
+]
